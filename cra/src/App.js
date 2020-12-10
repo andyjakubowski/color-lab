df9f675adf27bc7d4394b35e54e7417aad7a060d
Andy Jakubowski <hello@andyjakubowski.com>
2020-12-10 12:01:53 +0100

    Render new components in App

--- cra/src/App.js
+++ cra/src/App.js
@@ -4,6 +4,9 @@ import ComponentList from './components/ComponentList/ComponentList';
 import ButtonCTA from './components/showcase/ButtonCTA/ButtonCTA';
 import Label from './components/showcase/Label/Label';
 import Slider from './components/showcase/Slider/Slider';
+import Checkbox from './components/showcase/Checkbox/Checkbox';
+import Radio from './components/showcase/Radio/Radio';
+import ListItem from './components/showcase/ListItem/ListItem';
 import colorModes from './colorModeData';
 
 function App() {
@@ -15,7 +18,7 @@ function App() {
     'selected',
     'disabled',
   ];
-  const components = [ButtonCTA, Label, Slider];
+  const components = [ButtonCTA, Label, Slider, Checkbox, Radio, ListItem];
 
   return (
     <div className="App">
