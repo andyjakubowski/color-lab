016e1b06e844ff210a6f4121a9e8330159a4656d
Andy Jakubowski <hello@andyjakubowski.com>
2020-12-15 09:26:44 +0100

    Fix selected state for Checkbox

--- cra/src/Components/showcase/Checkbox/Checkbox.js
+++ cra/src/Components/showcase/Checkbox/Checkbox.js
@@ -1,6 +1,7 @@
 import './Checkbox.scss';
 import UnsupportedStatePlaceholder from '../../UnsupportedStatePlaceholder/UnsupportedStatePlaceholder';
 import getClassNames, { getColorModeClassName } from '../../../util/util';
+import React, { useState } from 'react';
 
 function Checkbox({ componentState, colorModeName }) {
   const supportedStates = [
@@ -11,19 +12,29 @@ function Checkbox({ componentState, colorModeName }) {
     'selected',
     'disabled',
   ];
+  const initialIsChecked = componentState === 'selected';
+  const [isChecked, setIsChecked] = useState(initialIsChecked);
   const isStateSupported = supportedStates.includes(componentState);
   const block = `Checkbox`;
   const elements = ['input'];
   const modifier = componentState;
   const classNames = getClassNames(block, elements, modifier);
   const colorModeClassName = getColorModeClassName(colorModeName);
+  const handleOnChange = function handleOnChange() {
+    setIsChecked(!isChecked);
+  };
 
   let returnElement;
 
   if (isStateSupported) {
     returnElement = (
       <div className={`${block} ${colorModeClassName}`}>
-        <input type="checkbox" className={classNames.input}></input>
+        <input
+          type="checkbox"
+          className={classNames.input}
+          checked={isChecked}
+          onChange={handleOnChange}
+        ></input>
       </div>
     );
   } else {
