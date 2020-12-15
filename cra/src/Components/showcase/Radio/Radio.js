5991947d743c16d232931cf43a946d034b59241d
Andy Jakubowski <hello@andyjakubowski.com>
2020-12-15 09:29:09 +0100

    Fix selected state for Radio

--- cra/src/Components/showcase/Radio/Radio.js
+++ cra/src/Components/showcase/Radio/Radio.js
@@ -1,6 +1,7 @@
 import './Radio.scss';
 import UnsupportedStatePlaceholder from '../../UnsupportedStatePlaceholder/UnsupportedStatePlaceholder';
 import getClassNames, { getColorModeClassName } from '../../../util/util';
+import React, { useState } from 'react';
 
 function Radio({ componentState, colorModeName }) {
   const supportedStates = [
@@ -11,19 +12,29 @@ function Radio({ componentState, colorModeName }) {
     'selected',
     'disabled',
   ];
+  const initialIsChecked = componentState === 'selected';
+  const [isChecked, setIsChecked] = useState(initialIsChecked);
   const isStateSupported = supportedStates.includes(componentState);
   const block = `Radio`;
   const elements = ['input'];
   const modifier = componentState;
   const classNames = getClassNames(block, elements, modifier);
   const colorModeClassName = getColorModeClassName(colorModeName);
+  const handleOnChange = function handleOnChange() {
+    setIsChecked(!isChecked);
+  };
 
   let returnElement;
 
   if (isStateSupported) {
     returnElement = (
       <div className={`${block} ${colorModeClassName}`}>
-        <input type="radio" className={classNames.input}></input>
+        <input
+          type="radio"
+          className={classNames.input}
+          checked={isChecked}
+          onChange={handleOnChange}
+        ></input>
       </div>
     );
   } else {
