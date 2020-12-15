6dbe8e137eb770dc0958a07aa3e18c2fc864a6db
Andy Jakubowski <hello@andyjakubowski.com>
2020-12-15 09:02:13 +0100

    Add UI states for ListItem

--- cra/src/Components/showcase/ListItem/ListItem.js
+++ cra/src/Components/showcase/ListItem/ListItem.js
@@ -1,29 +1,52 @@
 import './ListItem.scss';
 import UnsupportedStatePlaceholder from '../../UnsupportedStatePlaceholder/UnsupportedStatePlaceholder';
 import getClassNames, { getColorModeClassName } from '../../../util/util';
+import React, { useState } from 'react';
 
 function ListItem({ componentState, colorModeName }) {
   const TITLE_TEXT = 'Title';
   const SUBTITLE_TEXT = 'Subtitle';
-  const supportedStates = ['default'];
-  const isStateSupported = supportedStates.includes(componentState);
+  const [uiState, setUiState] = useState(componentState);
+  const supportedStates = [
+    'default',
+    'hovered',
+    'focused',
+    'pressed',
+    'selected',
+    'disabled',
+  ];
+  const isStateSupported = supportedStates.includes(uiState);
   const block = 'ListItem';
   const elements = ['container', 'separator', 'content', 'title', 'subtitle'];
-  const modifier = componentState;
+  const modifier = uiState;
   const classNames = getClassNames(block, elements, modifier);
   const colorModeClassName = getColorModeClassName(colorModeName);
+  const toggleSelectedState = function toggleSelectedState() {
+    if (uiState === 'default') {
+      setUiState('selected');
+    } else {
+      setUiState('default');
+    }
+  };
+  const handleClick = function handleClick() {
+    if (uiState !== 'default' && uiState !== 'selected') {
+      return;
+    }
+
+    toggleSelectedState();
+  };
 
   let returnElement;
 
   if (isStateSupported) {
     returnElement = (
-      <div className={`${block} ${colorModeClassName}`}>
+      <div className={`${block} ${colorModeClassName}`} onClick={handleClick}>
         <div className={classNames.container}>
-          <div className={classNames.separator}></div>
           <div className={classNames.content}>
             <div className={classNames.title}>{TITLE_TEXT}</div>
             <div className={classNames.subtitle}>{SUBTITLE_TEXT}</div>
           </div>
+          <div className={classNames.separator}></div>
         </div>
       </div>
     );
